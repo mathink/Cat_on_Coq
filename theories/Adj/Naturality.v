54df039ae1595a79bb2ee8ccfb7433919026f96f
mathink <mathink07@gmail.com>
2016-12-11 13:53:18 +0900

    add proof of naturality of adj_lr

--- theories/Adj/Naturality.v
+++ theories/Adj/Naturality.v
@@ -10,6 +10,42 @@ Set Universe Polymorphism.
 
 Require Import COC.Base.Main COC.Cons.Product COC.Adj.Adjunction.
 
+(** naturality of adj_lr **)
+Program Definition adj_lr_natrans_1 (C D: Category)(F: C --> D)(G: D --> C)(adj: F -| G)(c: C)
+  : Hom(F c, -) ==> (Hom(c, -) \o G) :=
+  [d :=> [f :-> adj_lr adj f]].
+Next Obligation.
+  now intros f g Heq; rewrite Heq.
+Qed.
+Next Obligation.
+  rewrite <- (cat_comp_id_dom (fmap G f \o _)), cat_comp_assoc.
+  rewrite <- adj_lr_naturality.
+  now rewrite fmap_id, cat_comp_id_dom.
+Qed.
+
+Program Definition op_functor (C D: Category)(F: C --> D)
+  : C^op --> D^op :=
+  [Functor by f :-> fmap F f ].
+Next Obligation.
+  - now apply fmap_proper.
+  - now apply fmap_comp.
+  - now apply fmap_id.
+Qed.
+Notation "F ^opF" := (op_functor F) (at level 0, format "F ^opF").
+
+Program Definition adj_lr_natrans_2 (C D: Category)(F: C --> D)(G: D --> C)(adj: F -| G)(d: D)
+  : (Hom(-, d) \o F^opF) ==> Hom(-, G d) :=
+  [c :=> [f :-> adj_lr adj f]].
+Next Obligation.
+  now intros f g Heq; rewrite Heq.
+Qed.
+Next Obligation.
+  rewrite <- (cat_comp_id_cod (_ \o f)).
+  rewrite <- fmap_id.
+  rewrite <- adj_lr_naturality.
+  now rewrite cat_comp_id_cod.
+Qed.
+
 (** naturality of adj_lr, adj_rl **)
 Program Definition prod_functor (C D C' D': Category)(F: C --> D)(F': C' --> D'): (product_category C C') --> (product_category D D') :=
   [ F \* F' with product_of_Cat].
@@ -28,16 +64,6 @@ Next Obligation.
   - now rewrite cat_comp_id_dom, cat_comp_id_cod.
 Qed.
 Notation "'HomF' C" := (hom_functor C) (at level 40, no associativity).
-
-Program Definition op_functor (C D: Category)(F: C --> D)
-  : C^op --> D^op :=
-  [Functor by f :-> fmap F f ].
-Next Obligation.
-  - now apply fmap_proper.
-  - now apply fmap_comp.
-  - now apply fmap_id.
-Qed.
-Notation "F ^opF" := (op_functor F) (at level 0, format "F ^opF").
 
 (** 
 #$Hom_D(-,-)\circ F\times Id_D$#
